7d0211dec60a5ca0e7fa27925a988cfa1a5403e1
Nour Z <nour@zibdie.com>
2023-01-10 02:51:46 -0500

    Bug fix and video data now captured

--- main.js
+++ main.js
@@ -9,24 +9,27 @@ const {
 if (!process.env.TIKTOK_PROFILE) {
   throw new Error("No TikTok profile provided");
 }
-/*
-if(!process.env.GOOGLE_TOKEN) {
-    throw new Error("No Google token provided");
+
+if (!process.env.GOOGLE_APPLICATION_CREDENTIALS) {
+  throw new Error("No Google token provided");
 }
-*/
+
+if (!process.env.GOOGLE_SHEET_ID) {
+  throw new Error("No Google Sheets ID provided");
+}
 
 (async () => {
   //console.log(`Getting recent TikToks from @${process.env.TIKTOK_PROFILE}`);
   //console.log(await GetRecentTiktoks(process.env.TIKTOK_PROFILE));
-
-  const spreadsheetId = "YOUR_SPREADSHEET_ID";
+  console.log(await DownloadTikTokByURL(process.env.TIKTOK_VIDEO));
+  /*
+  const spreadsheetId = process.env.GOOGLE_SHEETS_ID;
   const columnLetter = "A"; // column A
-  const value = "apple";
-  CheckGoogleSheetsColumn(auth, spreadsheetId, columnLetter, value).then(
-    (result) => {
-      console.log(
-        `All rows in column ${columnLetter} contain the value "${value}": ${result}`
-      );
-    }
-  );
+  const value = "john";
+  CheckGoogleSheetsColumn(spreadsheetId, columnLetter, value).then((result) => {
+    console.log(
+      `All rows in column ${columnLetter} contain the value "${value}": ${result}`
+    );
+  });
+  */
 })();
